f85732e81ee2adf9b856478b16e5e0d20189c83c
Nassim Fernane <nasfernane@gmail.com>
2023-06-07 22:52:45 +0200

    ajout data kepler et adaptation code pour getAllPlanets en promesse chargee avant lancement du serveur

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,47 @@
-const planets = ['Pluto'];
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+const isHabitable = (planet) => {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 // insolation flux
+    && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6; // planetory radius
+};
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+    // fs.createReadStream(__dirname + '../../../data/kepler_data.csv'))
+    .pipe(parse({
+      comment: '#',
+      columns: true
+    }))
+    .on('data', (planet) => {
+      if (isHabitable(planet)) habitablePlanets.push(planet);
+    })
+    .on('error', (err) => {
+      console.log('error:', err);
+      reject(err);
+    })
+    .on('end', () => {
+      console.log(`${habitablePlanets.length} habitable planets found !`)
+      // console.log(habitablePlanets.map((planet) => {
+      //   return planet['kepler_name'];
+      // }))
+
+      resolve();
+    })
+  })
+}
+
+
+
+
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets
+}
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,7 +1,6 @@
-const planets = require('../../models/planets.model');
+const { planets } = require('../../models/planets.model');
 
 function getAllPlanets(res, res) {
-  console.log('planets req')
   return res.status(200).json(planets);
 }
 
