5ed9f7f1a47987c399d37ab17c3d41a3867a7e27
Nassim Fernane <nasfernane@gmail.com>
2023-07-09 10:24:49 +0200

    ajout jests et base structure de tests sur launches

---
+++ server/src/routes/launches/launches.spec.js
@@ -0,0 +1,25 @@
+describe('Test GET /launches', () => {
+  test('It should respond with 200 success', () => {
+    const response = 200;
+    expect(response).toBe(200);
+  })
+})
+
+
+describe('Test POST /launch', () => {
+  test('It should respond with 200 success', () => {
+    const response = 200;
+    expect(response).toBe(200);
+  })
+
+  test('It should catch missing required properties', () => {
+    const response = 400;
+    expect(response).toBe(400);
+  })
+
+  test('It should catch invalid dates', () => {
+    const response = 400;
+    expect(response).toBe(400);
+  })
+
+})
