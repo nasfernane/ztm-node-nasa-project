25246c5082aaf8338d63bf9b3529de2e12567231
Nassim Fernane <nasfernane@gmail.com>
2023-07-29 23:23:30 +0200

    mise en version api

--- server/src/routes/launches/launches.spec.js
+++ server/src/routes/launches/launches.spec.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('./../../services/mongo');
 
+const API_VERSION = "/v1";
 
 describe('Test launches API', () => {
   beforeAll(async () => {
@@ -16,7 +17,7 @@ describe('Test launches API', () => {
     test('It should respond with 200 success', async () => {
       await mongoConnect();
       await request(app)
-        .get('/launches')
+        .get(API_VERSION + '/launches')
         .expect('Content-Type', /json/)
         .expect(200);
     })
@@ -45,7 +46,7 @@ describe('Test launches API', () => {
 
     test('It should respond with 201 created', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post(API_VERSION + '/launches')
         .send(completeLaunchData)
         .expect('Content-Type', /json/)
         .expect(201);
@@ -59,7 +60,7 @@ describe('Test launches API', () => {
 
     test('It should catch missing required properties', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post(API_VERSION + '/launches')
         .send(launchDataWithouthDate)
         .expect('Content-Type', /json/)
         .expect(400);
@@ -71,7 +72,7 @@ describe('Test launches API', () => {
 
     test('It should catch invalid date', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post(API_VERSION + '/launches')
         .send(launchDataWithInvalidDate)
         .expect('Content-Type', /json/)
         .expect(400);
